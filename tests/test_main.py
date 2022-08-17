b61f1272136af7410a04bd4da934064bfcce9ca1
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-08-17 18:59:47 +0200

    fixing variance_entropy_clean

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,15 +6,25 @@ def test_pretrained():
     model = torchvision.models.resnet18(True)
     quality_dict = measure_quality(model)
 
+    from pprint import pprint
+    pprint(quality_dict)
+
     assert quality_dict is not None
 
     for layer_name, entry in quality_dict.items():
         assert "sparsity" in entry
         assert "variance_entropy" in entry
+        assert "variance_entropy_clean" in entry
+        assert "n" in entry
         assert entry["sparsity"] <= 1
         assert entry["sparsity"] >= 0
         assert entry["variance_entropy"] >= 0
         assert entry["variance_entropy"] <= 1
+        assert entry["variance_entropy_clean"] >= 0
+        assert entry["variance_entropy_clean"] <= 1
+
+        if entry["sparsity"] == 0:
+            assert entry["variance_entropy"] == entry["variance_entropy_clean"]
 
 
 def test_untrained():
@@ -26,8 +36,11 @@ def test_untrained():
     for layer_name, entry in quality_dict.items():
         assert "sparsity" in entry
         assert "variance_entropy" in entry
+        assert "variance_entropy_clean" in entry
+        assert "n" in entry
         assert entry["sparsity"] >= 0
 
         # conservative thresholds
         assert entry["sparsity"] <= 0.1
         assert entry["variance_entropy"] >= 0.9
+        assert entry["variance_entropy_clean"] >= 0.9
--- torchconvquality/__init__.py
+++ torchconvquality/__init__.py
@@ -8,37 +8,32 @@ def _entropy_max_threshold(n):
     return l / (1 + np.exp(-k * (np.log2(n) - x0))) + b
 
 
+def svd_variance(x):
+    s = np.linalg.svd(x - x.mean(axis=0), full_matrices=False, compute_uv=False)
+    variance = s ** 2 / (len(x) - 1)
+    return variance
+
+
 def measure_quality(model, sparsity_eps=0.01):
     info_dict = {}
     for name, conv_layer in filter(
             lambda x: type(x[1]) == torch.nn.Conv2d and x[1].kernel_size == (3, 3), model.named_modules()):
         info_dict[name] = {}
-
         w = conv_layer.weight.detach().cpu().numpy().reshape(-1, conv_layer.kernel_size[0] * conv_layer.kernel_size[1])
-
         n = w.shape[0]
-
-        s = np.linalg.svd(w - w.mean(axis=0), full_matrices=False, compute_uv=False)
-        v = s ** 2 / (n - 1)
-        entropy = scipy.stats.entropy(v, base=10) / _entropy_max_threshold(n)
-
         info_dict[name]["n"] = n
-        info_dict[name]["variance_entropy"] = entropy
 
         t = np.abs(w).max() * sparsity_eps
         new_layer = np.ones_like(w)
         new_layer[np.abs(w) < t] = 0
         sparsity = (new_layer.sum(axis=1) == 0).sum() / n
-
         info_dict[name]["sparsity"] = sparsity
-        
-        norms = np.linalg.norm(w, axis=0)
-        
-        w = w[norms >= sparsity_eps * norms.max()]
-        
-        s = np.linalg.svd(w - w.mean(axis=0), full_matrices=False, compute_uv=False)
-        v = s ** 2 / (n - 1)
-        entropy = scipy.stats.entropy(v, base=10) / _entropy_max_threshold(n)
-        info_dict[name]["variance_entropy_clean"] = entropy
+
+        entropy = scipy.stats.entropy(svd_variance(w), base=10) / _entropy_max_threshold(n)
+        info_dict[name]["variance_entropy"] = entropy
+
+        w_clean = w[new_layer.sum(axis=1) > 0]
+        entropy_clean = scipy.stats.entropy(svd_variance(w_clean), base=10) / _entropy_max_threshold(len(w_clean))
+        info_dict[name]["variance_entropy_clean"] = entropy_clean
 
     return info_dict
