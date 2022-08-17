686fca2b517c41407147853c0df179d69db92495
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-08-17 20:25:46 +0200

    adding option to individually measure layers

--- torchconvquality/__init__.py
+++ torchconvquality/__init__.py
@@ -14,26 +14,31 @@ def svd_variance(x):
     return variance
 
 
+def measure_layer_quality(conv_layer, sparsity_eps):
+    info_dict = {}
+    w = conv_layer.weight.detach().cpu().numpy().reshape(-1, conv_layer.kernel_size[0] * conv_layer.kernel_size[1])
+    n = w.shape[0]
+    info_dict["n"] = n
+
+    t = np.abs(w).max() * sparsity_eps
+    new_layer = np.ones_like(w)
+    new_layer[np.abs(w) < t] = 0
+    sparsity = (new_layer.sum(axis=1) == 0).sum() / n
+    info_dict["sparsity"] = sparsity
+
+    entropy = scipy.stats.entropy(svd_variance(w), base=10) / _entropy_max_threshold(n)
+    info_dict["variance_entropy"] = entropy
+
+    w_clean = w[new_layer.sum(axis=1) > 0]
+    entropy_clean = scipy.stats.entropy(svd_variance(w_clean), base=10) / _entropy_max_threshold(len(w_clean))
+    info_dict["variance_entropy_clean"] = entropy_clean
+    return info_dict
+
+
 def measure_quality(model, sparsity_eps=0.01):
     info_dict = {}
     for name, conv_layer in filter(
             lambda x: type(x[1]) == torch.nn.Conv2d and x[1].kernel_size == (3, 3), model.named_modules()):
-        info_dict[name] = {}
-        w = conv_layer.weight.detach().cpu().numpy().reshape(-1, conv_layer.kernel_size[0] * conv_layer.kernel_size[1])
-        n = w.shape[0]
-        info_dict[name]["n"] = n
-
-        t = np.abs(w).max() * sparsity_eps
-        new_layer = np.ones_like(w)
-        new_layer[np.abs(w) < t] = 0
-        sparsity = (new_layer.sum(axis=1) == 0).sum() / n
-        info_dict[name]["sparsity"] = sparsity
-
-        entropy = scipy.stats.entropy(svd_variance(w), base=10) / _entropy_max_threshold(n)
-        info_dict[name]["variance_entropy"] = entropy
-
-        w_clean = w[new_layer.sum(axis=1) > 0]
-        entropy_clean = scipy.stats.entropy(svd_variance(w_clean), base=10) / _entropy_max_threshold(len(w_clean))
-        info_dict[name]["variance_entropy_clean"] = entropy_clean
+        info_dict[name] = measure_layer_quality(conv_layer, sparsity_eps)
 
     return info_dict
