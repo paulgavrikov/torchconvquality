36ffeb0cb702efc4796483369d20e5e19c178044
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-09-11 11:38:38 +0000

    disentangling code

--- torchconvquality/__init__.py
+++ torchconvquality/__init__.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 
-def torch_entropy10(probs):
+def _torch_entropy10(probs):
     e = 0
     for p in probs:
         e += p * torch.log10(p)
@@ -14,7 +14,7 @@ def _entropy_max_threshold(n):
     return l / (1 + np.exp(-k * (np.log2(n) - x0))) + b
 
 
-def svd_variance_ratio(x):
+def _svd_variance_ratio(x):
     print(x.shape)
     s = torch.linalg.svdvals(x - x.mean(axis=0))
     print(s)
@@ -22,31 +22,40 @@ def svd_variance_ratio(x):
     return variance / variance.sum()
 
 
-def measure_layer_quality(conv_layer, sparsity_eps):
+def sparsity(w, sparsity_eps=0.01, **kwargs):
+    n = w.shape[0]
+    t = abs(w).max() * sparsity_eps
+    new_layer = torch.ones_like(w)
+    new_layer[abs(w) < t] = 0
+    sparse_mask = (new_layer.sum(axis=1) == 0)
+    sparsity = sparse_mask.sum() / n
+    return sparsity.item(), sparse_mask
+
+
+def variance_entropy(w, **kwargs):
+    n = w.shape[0]
+    ratio = _svd_variance_ratio(w)
+    entropy = _torch_entropy10(ratio) / _entropy_max_threshold(n)
+    return entropy.item()
+
+
+def measure_layer_quality(conv_layer, **kwargs):
     info_dict = {}
     w = conv_layer.weight.view(-1, conv_layer.kernel_size[0] * conv_layer.kernel_size[1])
     n = w.shape[0]
     info_dict["n"] = n
 
-    t = abs(w).max() * sparsity_eps
-    new_layer = torch.ones_like(w)
-    new_layer[abs(w) < t] = 0
-    sparsity = (new_layer.sum(axis=1) == 0).sum() / n
-    info_dict["sparsity"] = sparsity.item()
-
-    entropy = torch_entropy10(svd_variance_ratio(w)) / _entropy_max_threshold(n)
-    info_dict["variance_entropy"] = entropy.item()
-
-    w_clean = w[new_layer.sum(axis=1) > 0]
-    entropy_clean = torch_entropy10(svd_variance_ratio(w_clean)) / _entropy_max_threshold(len(w_clean))
-    info_dict["variance_entropy_clean"] = entropy_clean.item()
+    sparsity_ratio, sparse_mask = sparsity(w, **kwargs)
+    info_dict["sparsity"] = sparsity_ratio
+    info_dict["variance_entropy"] = variance_entropy(w, **kwargs)
+    info_dict["variance_entropy_clean"] = variance_entropy(w[~sparse_mask], **kwargs)
     return info_dict
 
 
-def measure_quality(model, sparsity_eps=0.01):
+def measure_quality(model, **kwargs):
     info_dict = {}
     for name, conv_layer in filter(
             lambda x: type(x[1]) == torch.nn.Conv2d and x[1].kernel_size == (3, 3), model.named_modules()):
-        info_dict[name] = measure_layer_quality(conv_layer, sparsity_eps)
+        info_dict[name] = measure_layer_quality(conv_layer, **kwargs)
 
     return info_dict
